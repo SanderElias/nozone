5bd4b96d5cfff27a552effbd51c3e32474c7fd4f
Sander Elias <sanderelias+git@gmail.com>
2019-10-21 15:39:19 +0200

    added more graphs

--- src/app/tickGraph.component.ts
+++ src/app/tickGraph.component.ts
@@ -1,88 +1,127 @@
 import {
-    Component,
-    Input,
-    ApplicationRef,
-    ChangeDetectionStrategy,
-    ViewChild,
-    ElementRef,
-    ChangeDetectorRef
+  Component,
+  Input,
+  ApplicationRef,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
 } from '@angular/core';
 
-import { timer } from 'rxjs/observable/timer';
-import {
-    take,
-    tap,
-    debounce,
-    debounceTime,
-    throttleTime
-} from 'rxjs/operators';
-import { TickService } from './samples/tick.service';
-import { interval } from 'rxjs/observable/interval';
-import { Scheduler } from 'rxjs/Scheduler';
-import { Subject } from 'rxjs/Subject';
-import { takeUntil } from 'rxjs/operators/takeUntil';
-import { combineLatest } from 'rxjs/observable/combineLatest';
+import {timer} from 'rxjs/observable/timer';
+import {take, tap, debounce, debounceTime, throttleTime} from 'rxjs/operators';
+import {TickService} from './samples/tick.service';
+import {interval} from 'rxjs/observable/interval';
+import {Scheduler} from 'rxjs/Scheduler';
+import {Subject} from 'rxjs/Subject';
+import {takeUntil} from 'rxjs/operators/takeUntil';
+import {combineLatest} from 'rxjs/observable/combineLatest';
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'tick-graph',
-    template: `
+  // tslint:disable-next-line:component-selector
+  selector: 'tick-graph',
+  template: `
     <button (click)="stop$.next('')">stop</button>
     <button (click)="remove(ticks[0])">remove</button>
     <button (click)="add()">add</button>
-    <input type="checkbox" #box>
-    <br>
-    <br>
+    <input type="checkbox" #box />
+    <br />
+    <br />
     <div class="tickHolder">
-    <ng-container *ngIf="box.checked">
-      <show-tick *ngFor="let tick$ of ticks" [data]="tick$|async" ></show-tick>
-    </ng-container>
-    <ng-container *ngIf="!box.checked">
-      <show-tick-bar *ngFor="let tick$ of ticks" [data]="tick$|async" ></show-tick-bar>
-    </ng-container>
+      <ng-container *ngIf="box.checked">
+        <show-tick
+          *ngFor="let tick$ of ticks"
+          [data]="tick$ | async"
+        ></show-tick>
+      </ng-container>
+      <ng-container *ngIf="!box.checked">
+        <show-tick-bar
+          *ngFor="let tick$ of ticks"
+          [data]="tick$ | async"
+        ></show-tick-bar>
+      </ng-container>
     </div>
-
-     `,
-    changeDetection: ChangeDetectionStrategy.Default,
-    styles: [
-        `
+  `,
+  changeDetection: ChangeDetectionStrategy.Default,
+  styles: [
+    `
       .tickHolder {
-        display:grid;
-        grid-template-columns: repeat(auto-fill, 305px) ;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 305px);
         grid-auto-rows: 1fr;
       }
-    `
-    ]
+    `,
+  ],
 })
 export class TickGraphComponent {
-    stop$ = new Subject();
-    counter$ = timer(0, 1000 / 30);
-    lock = false;
+  stop$ = new Subject();
+  counter$ = timer(0, 1000 / 30);
+  lock = false;
 
-    @Input() name: string;
+  @Input() name: string;
 
-    ticks = [
-        this.t.tick$.pipe(takeUntil(this.stop$)),
-        this.t.tick$.pipe(takeUntil(this.stop$)),
-        this.t.tick$.pipe(takeUntil(this.stop$))
-    ];
+  ticks = [
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+      this.t.tick$.pipe(takeUntil(this.stop$)),
+];
 
-    constructor(
-        private app: ApplicationRef,
-        private ref: ChangeDetectorRef,
-        private t: TickService
-    ) {
-        this.counter$
-            .pipe(tap(_ => this.ref.detectChanges()), takeUntil(this.stop$))
-            .subscribe();
-    }
+  constructor(
+    private app: ApplicationRef,
+    private ref: ChangeDetectorRef,
+    private t: TickService
+  ) {
+    this.counter$
+      .pipe(
+        tap(_ => this.ref.detectChanges()),
+        takeUntil(this.stop$)
+      )
+      .subscribe();
+  }
 
-    remove(ticker) {
-        const i = this.ticks.indexOf(ticker);
-        this.ticks.splice(i, 1);
-    }
+  remove(ticker) {
+    const i = this.ticks.indexOf(ticker);
+    this.ticks.splice(i, 1);
+  }
 
-    add() {
-        this.ticks.push(this.t.tick$.pipe(takeUntil(this.stop$)));
-    }
+  add() {
+    this.ticks.push(this.t.tick$.pipe(takeUntil(this.stop$)));
+  }
 }
